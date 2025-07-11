d8e50f5930e611e43ed4987627c59771d0006a4e
AndreaCatapano <a.catapano96@gmail.com>
2025-07-12 01:40:55 +0200

    feat: completed step-1

---
+++ src/main/java/org/lessons/java/best_of_the_year/controller/BestOfTheYearController.java
@@ -0,0 +1,19 @@
+package org.lessons.java.best_of_the_year.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/")
+@Controller
+public class BestOfTheYearController {
+
+    @GetMapping("/")
+    public String home(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute(name);
+        return "index";
+    }
+
+}
